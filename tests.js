515c4a8afb84b28616807302f4b61f09da41554d
vmik7 <vmik7@yandex.ru>
2021-03-23 14:09:57 +0300

    Починил тесты

--- tests.js
+++ tests.js
@@ -33,31 +33,57 @@ let validateSlide = (slide) => {
 }
 
 // Функция проверки равенства слайдов и просто объектов в некотором случае
-let compareObjects = (correctObject, myObject, specialKey, specialHandler) => {
-    if (correctObject === null || myObject === null) {
+let compareSlides = (correctSlide, mySlide, specialRules = []) => {
+    if (typeof correctSlide !== 'object' || correctSlide === null || typeof mySlide !== 'object' || mySlide === null) {
+        console.log('ERROR! Objects are not correct! - function \'compareSlides\'');
+        return false;
+    }
+    for (key in correctSlide.data) {
+        let rule = specialRules.find(item => item.key === key);
+        if (rule) {
+            if (!rule.handler(correctSlide.data[key], mySlide.data[key])) {
+                console.log(`ERROR! some data in '${ key }' does not match...`);
+                return false;
+            }
+        }
+        else {
+            if (correctSlide.data[key] !== mySlide.data[key]) {
+                console.log(`ERROR! key '${ key }' does not match!`);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Функция сравнения объектов без вложенности
+let compareObjects = (correctObject, myObject) => {
+    if (typeof correctObject !== 'object' || correctObject === null || typeof myObject !== 'object' || myObject === null) {
+        console.log('ERROR! Objects are not correct! - function \'compareObjects\'');
         return false;
     }
     for (key in correctObject) {
-        if (key === specialKey) {
-            return specialHandler(correctObject[key], myObject[key]);
-        }
-        else {
-            return correctObject[key] === myObject[key];
+        if (correctObject[key] !== myObject[key]) {
+            console.log(`ERROR! key '${ key }' does not match!`);
+            return false;
         }
     }
+    return true;
 }
 
 // Функция сравнения двух массивов
 let compareArrays = (correctArray, myArray) => {
     if (!Array.isArray(correctArray) || !Array.isArray(myArray)) {
+        console.log('ERROR! Arrays are not correct! - function \'compareArrays\'');
         return false;
     }
-    return correctArray.length === myArray.length && correctArray.every((item, index) => item === myArray);
+    return correctArray.length === myArray.length && correctArray.every((item, index) => item === myArray[index]);
 }
 
 // Обработчик для сравнения массива объектов (для поля users в шаблоне leaders к примеру)
 let arrayHandler = (correctArray, myArray) => {
     if (!Array.isArray(correctArray) || !Array.isArray(myArray)) {
+        console.log('ERROR! Arrays are not correct! - function \'arrayHandler\'');
         return false;
     }
     return correctArray.length === myArray.length && correctArray.every((item, index) => compareObjects(item, myArray[index]));
@@ -66,10 +92,12 @@ let arrayHandler = (correctArray, myArray) => {
 // Обработчик для сравнения объектов data в шаблоне activity
 let activityHandler = (correctObject, myObject) => {
     if (correctObject === null || myObject === null) {
+        console.log('ERROR! Objects are not correct! - function \'activityHandler\'');
         return false;
     }
     for (day in correctObject) {
         if (!compareArrays(correctObject[day], myObject[day])) {
+            console.log(`ERROR! Arrays are not match on '${ day }'!`);
             return false;
         }
     }
@@ -80,35 +108,35 @@ let activityHandler = (correctObject, myObject) => {
 console.log('\n\x1b[37mLeaders: \x1b[0m');
 let correctLeaders = correctData.find(item => item.alias === 'leaders');
 let myLeaders = myData.find(item => item.alias === 'leaders');
-let leadersOK = validateSlide(myLeaders) && compareObjects(correctLeaders, myLeaders, 'users', arrayHandler);
+let leadersOK = validateSlide(myLeaders) && compareSlides(correctLeaders, myLeaders, [ { key: 'users', handler: arrayHandler } ]);
 console.log(leadersOK ? '\x1b[32mOK\x1b[0m' : '\x1b[31mFAIL\x1b[0m');
 
 // Сравниваем шаблон vote
 console.log('\n\x1b[37mVote: \x1b[0m');
 let correctVote = correctData.find(item => item.alias === 'vote');
 let myVote = myData.find(item => item.alias === 'vote');
-let voteOK = validateSlide(myVote) && compareObjects(correctVote, myVote, 'users', arrayHandler);
+let voteOK = validateSlide(myVote) && compareSlides(correctVote, myVote, [ { key: 'users', handler: arrayHandler } ]);
 console.log(voteOK ? '\x1b[32mOK\x1b[0m' : '\x1b[31mFAIL\x1b[0m');
 
 // Сравниваем шаблон chart
 console.log('\n\x1b[37mChart: \x1b[0m');
 let correctChart = correctData.find(item => item.alias === 'chart');
 let myChart = myData.find(item => item.alias === 'chart');
-let chartOK = validateSlide(myChart) && compareObjects(correctChart, myChart, 'values', arrayHandler);
+let chartOK = validateSlide(myChart) && compareSlides(correctChart, myChart, [ { key: 'values', handler: arrayHandler }, { key: 'users', handler: arrayHandler } ]);
 console.log(chartOK ? '\x1b[32mOK\x1b[0m' : '\x1b[31mFAIL\x1b[0m');
 
 // Сравниваем шаблон diagram
 console.log('\n\x1b[37mDiagram: \x1b[0m');
 let correctDiagram = correctData.find(item => item.alias === 'diagram');
 let myDiagram = myData.find(item => item.alias === 'diagram');
-let diagramOK = validateSlide(myDiagram) && compareObjects(correctDiagram, myDiagram, 'categories', arrayHandler);
+let diagramOK = validateSlide(myDiagram) && compareSlides(correctDiagram, myDiagram, [ { key: 'categories', handler: arrayHandler } ]);
 console.log(diagramOK ? '\x1b[32mOK\x1b[0m' : '\x1b[31mFAIL\x1b[0m');
 
 // Сравниваем шаблон activity
 console.log('\n\x1b[37mActivity: \x1b[0m');
 let correctActivity = correctData.find(item => item.alias === 'activity');
 let myActivity = myData.find(item => item.alias === 'activity');
-let activityOK = validateSlide(myActivity) && compareObjects(correctActivity, myActivity, 'data', activityHandler);
+let activityOK = validateSlide(myActivity) && compareSlides(correctActivity, myActivity, [ { key: 'data', handler: activityHandler } ]);
 console.log(activityOK ? '\x1b[32mOK\x1b[0m' : '\x1b[31mFAIL\x1b[0m');
 
 if (leadersOK && voteOK && chartOK && diagramOK && activityOK) {
